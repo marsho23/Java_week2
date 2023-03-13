b75e83016a3ab5691e7bd066124fe4d163378a42
Admin <Admin@192.168.1.100>
2023-03-13 12:32:08 +0000

    added flowchart2 exercise

---
+++ src/com/qa/iterations/Flowchart2.java
@@ -0,0 +1,20 @@
+package com.qa.iterations;
+
+public class Flowchart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int A = 100;
+		while (A <= 200) {
+			if (A % 2 == 0)
+				System.out.println("-");
+			else
+				System.out.println("*");
+
+			A++;
+		}
+
+	}
+
+}
