758ac7438d29d30690c3ebb6660f99abeb62580e
Admin <Admin@192.168.1.100>
2023-03-13 14:11:05 +0000

    added exercise5

---
+++ src/com/qa/iterations/Exercise5.java
@@ -0,0 +1,18 @@
+package com.qa.iterations;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printNumbers();
+	}
+
+	public static void printNumbers() {
+		for (int A = 1; A <= 10; A++) {
+			for (int i = 1; i <= A; i++) {
+				System.out.println(A);
+			}
+		}
+	}
+
+}
